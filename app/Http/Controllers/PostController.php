4bb1e5cb8c6d407ca2a75c7e581e356fcd0e0a04
Surinder Singh <surindersingh@ucreate.co.in>
2018-08-21 14:43:31 +0530

    test rollbar integration

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function index()
     {
-       
+       \Log::debug('Test debug message');
         $posts = Post::all();
 
         return view('posts/index', compact('posts'));
