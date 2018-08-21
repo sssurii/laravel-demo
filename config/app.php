6450459343d62815f323cf25c3bb6b927dbb75bd
Surinder Singh <surindersingh@ucreate.co.in>
2018-08-21 13:18:52 +0530

    add rollbar package

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rollbar\Laravel\RollbarServiceProvider::class,
 
         /*
          * Application Service Providers...
