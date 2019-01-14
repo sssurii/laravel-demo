9da53d1ecbc6046417be9476230fa01aa72448f6
Surinder Singh <surindersingh@ucreate.co.in>
2019-01-14 17:21:00 +0530

    add HTTP/HTTPS enable settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{url('css/app.css',[],env('HTTPS_ENABLE', true)) }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -46,7 +46,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            <li><a href="{{url('/posts')}}">Posts</a></li>
+                            <li><a href="{{url('/posts',[],env('HTTPS_ENABLE', true)) }}">Posts</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -76,6 +76,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{url('js/app.js',[],env('HTTPS_ENABLE', true)) }}"></script>
 </body>
 </html>
